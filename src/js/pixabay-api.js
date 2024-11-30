2a7ab08f7cec553c4fa96732b48ceec17a92f2a0
Vladosha <vladaservetnik@gmail.com>
2024-11-30 17:18:54 +0200

    hw11-app-done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,13 +1,23 @@
 
-export const httpSearch = (name) => {
+
+export const imagesSearch = (name) => {
     const apiKey = "47345226-0bb2374200c7482689dd59086";
-    fetch(`https://pixabay.com/api/?key=${apiKey}&q=${name}`)
+    const serchParams = new URLSearchParams({
+        key: apiKey,
+        q: name,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+    });
+
+   return fetch(`https://pixabay.com/api/?${serchParams}`)
         .then((response) => {
             if (!response.ok) {
-            throw new Error(response.status)
+                throw new Error(response.status)
             }
             return response.json();
         })
-    .then((res) => console.log(res))
-    .catch((er) => console.log(er))
+        .then((data) =>  data.hits)   
 }
+
+
--- src/main.js
+++ src/main.js
@@ -1,29 +1,60 @@
 
-import { httpSearch } from "./js/pixabay-api"
+import { imagesSearch } from "./js/pixabay-api"
+import { renderImages } from "./js/render-functions"
+
 
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import errorImage from './img/error.png';
+
 const form = document.querySelector(".form");
+const gallery = document.querySelector(".gallery");
+const loader = document.querySelector(".loader")
 
 form.addEventListener("submit", handleSearch);
 
 function handleSearch(event) {
     event.preventDefault();
-    const inputSearch = event.target.elements.name.value;
+    gallery.innerHTML = "";
+    let inputElement = event.target.elements.name;
+    const inputSearch = inputElement.value.trim();
     if (!inputSearch) {
         iziToast.show({
-                        title: '',
-                        backgroundColor: 'red',
-                        messageColor: 'white',
-                        message: `Add data for search`,
-                        position: 'topRight',
-                    }
-        )
+            title: '',
+            iconUrl: `${errorImage}`,
+            backgroundColor: 'red',
+            messageColor: 'white',
+            message: `Enter the data for the search!`,
+            position: 'topRight',
+        }
+        );
         return;
-    }
-    httpSearch(inputSearch)
-    //     .then((res) => console.log(res))
-    // .catch((er) => console.log(er))
-    
+    };
+    imagesSearch(inputSearch)
+        .then((arrImg) => {
+            if (arrImg.length === 0) {
+                iziToast.show({
+                    title: '',
+                    iconUrl: `${errorImage}`,
+                    backgroundColor: 'red',
+                    messageColor: 'white',
+                    message: `Sorry, there are no images matching your search query. Please try again!`,
+                    position: 'topRight',
+                })
+            };
+            loader.style.display = "inline-block";
+            gallery.insertAdjacentHTML("beforeend", renderImages(arrImg));
+            const lightbox = new SimpleLightbox('.gallery a', {
+                captionsData: 'alt',
+                captionDelay: 250,
+            });
+            lightbox.refresh();
+            inputElement.value = "";
+            loader.style.display = "none";
+        })
+        .catch((error) => console.log(error))
 }
